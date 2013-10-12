6741248816211d5b71695fe78e9480a406c3a8fd
Nick Cottrell <nick.a.cottrell@gmail.com>
2013-10-12 10:07:55 -0700

    uh

--- js/access-data.js
+++ js/access-data.js
@@ -8,17 +8,11 @@
 
 
 $(document).ready(function(){
-$.getJSON( "http://waytoosick.com/rcrds-api/nowhite.json", function( data ) {
-  alert("NAME=" + data.name);
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+	$.getJSON("http://waytoosick.com/rcrds-api/nowhite.json", function (jsonobj) {
+    	alert("NAME=" + jsonobj.name);
+	});
 });
-});
+
+
+
 
