ecebe4ed75365b162137cdf7e7a7511e74896aa8
Nick Cottrell <nick.a.cottrell@gmail.com>
2013-10-12 10:11:50 -0700

    uh

--- js/access-data.js
+++ js/access-data.js
@@ -9,6 +9,7 @@
 
 $(document).ready(function(){
 	$.getJSON("http://waytoosick.com/rcrds-api/nowhite.json", function (jsonobj) {
+    	alert(jsonobj);
     	alert("NAME=" + jsonobj.name);
 	});
 });
